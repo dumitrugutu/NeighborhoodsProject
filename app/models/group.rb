9ddb2f6b44ab8c39e9d0a2ac2c2ed7ae84a85adf
McEileen <eileenm@gmail.com>
2016-04-26 12:26:53 -0500

    switch belong_has_to_many relationships has_many through relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ActiveRecord::Base
-  has_and_belongs_to_many :neighborhoods, foreign_key: :group_id
+  has_many :neighborhoods, through: :service_areas
   has_many :group_reps, foreign_key: :group_id
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,3 +1,3 @@
 class Neighborhood < ActiveRecord::Base
-  has_and_belongs_to_many :groups, foreign_key: :neighborhood_id
+  has_many :groups, through: :service_areas
 end
