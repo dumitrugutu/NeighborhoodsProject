744072d7d0fc47d9366f915e3fd84f69116898d4
McEileen <eileenm@gmail.com>
2016-04-26 13:22:29 -0500

    create seed file

--- db/migrate/20160425172332_create_group_reps.rb
+++ db/migrate/20160425172332_create_group_reps.rb
@@ -4,7 +4,7 @@ class CreateGroupReps < ActiveRecord::Migration
       t.references :group, index: true, foreign_key: true
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
 
       t.timestamps null: false
     end
