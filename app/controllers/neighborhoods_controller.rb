d688f4722852a2d5637d70648d8c877d4e29eeb2
McEileen <eileenm@gmail.com>
2016-04-26 14:49:19 -0500

    spike unsuccessfully on search bar before new approach

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -5,10 +5,14 @@ class NeighborhoodsController < ApplicationController
   def search
     @q = "%#{params[:query]}%"
     @neighborhoods = Neighborhood.where("name LIKE ?", @q)
-    render 'index'
+    render 'show'
   end
 
   def show
+    p "------------------------------below you can see params[:id]"
+    p params[:id]
+    p "BELOW YOU CAN SEE ID!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    p id
     @neighborhood = Neighborhood.find_by(id: params[:id])
     @groups = @neighborhood.groups
   end
