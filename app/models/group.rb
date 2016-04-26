fcf8ef90aab989100133ab2c1866cf7fd97ec671
McEileen <eileenm@gmail.com>
2016-04-25 22:19:01 -0500

    update models to reflect relationships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ActiveRecord::Base
-  has_many :neighborhoods, foreign_key: :group_id
+  has_and_belongs_to_many :neighborhoods, foreign_key: :group_id
+  has_many :group_reps, foreign_key: :group_id
 end
