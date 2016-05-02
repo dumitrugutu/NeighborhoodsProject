6c01627c2f9dd63088ffa232c1d68c0c7a8aff21
Bryan Karlovitz <karlovitz@u.northwestern.edu>
2016-05-02 17:32:44 +0000

    rspec test for event model

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Event do
+  context 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:organizer_contact_info) }
+    it { should validate_presence_of(:group_rep_id) }
+    it { should validate_presence_of(:event_time) }
+    # note: don't need to validate :is_free because it has a default value
+  end
+  
+  context 'Associations' do
+    it { should belong_to(:group_rep) }
+  end
+end
