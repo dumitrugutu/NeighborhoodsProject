ac2ee904d7a47ce2dddce7d2c96404225e619937
Bryan Karlovitz <karlovitz@u.northwestern.edu>
2016-05-02 18:43:27 +0000

    rspec test for GroupRep model

--- app/models/group_rep.rb
+++ app/models/group_rep.rb
@@ -1,8 +1,9 @@
 class GroupRep < ActiveRecord::Base
   belongs_to :group
   has_many :events, foreign_key: :group_rep_id
+  
+  validates_presence_of :name, :email, :password_digest, :group_id
   validates_format_of :email, :with => /@/
 
   has_secure_password
-
 end
