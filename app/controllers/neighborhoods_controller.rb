4dd1b5d0eb6cecdbf8254fcbfbf023e8dbb17330
McEileen <eileenm@gmail.com>
2016-05-03 23:21:43 -0500

    get individ show pages to work, issues w search still

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -3,18 +3,19 @@ class NeighborhoodsController < ApplicationController
   end
 
   def search
+    p "QUERY BELOW_______________"
+    p params[:query]
     @q = "%#{params[:query]}%"
-    @neighborhoods = Neighborhood.where("name LIKE ?", @q)
+    p "neighborhood below __________"
+    p @neighborhood = Neighborhood.where("name LIKE ?", @q)
+    p "@neighborhood.groups below ____________________"
+    p @neighborhood.groups
     render 'show'
   end
 
   def show
-    #p "------------------------------below you can see params[:id]"
-    #p params[:id]
-    #p "BELOW YOU CAN SEE ID!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    #p id
-    #@neighborhood = Neighborhood.find_by(id: params[:id])
-    #@groups = @neighborhood.groups
+    @neighborhood = Neighborhood.find_by(id: params[:id])
+    @groups = @neighborhood.groups
   end
 
   private
