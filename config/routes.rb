cf0b0198b7c896df18f3b84a822f789c6fb09c6e
McEileen <eileenm@gmail.com>
2016-05-06 13:27:08 -0500

    grouprep can register but is not successfully connected to group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :group_reps
+  devise_for :group_reps, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
