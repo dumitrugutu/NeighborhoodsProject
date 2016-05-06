66cc2b0bfef55de565628933a7598ac6fb6fd549
McEileen <eileenm@gmail.com>
2016-05-06 09:27:21 -0500

    add rspec tests Bryan wrote back into GroupRep test file

--- spec/models/group_rep_spec.rb
+++ spec/models/group_rep_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe GroupRep, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:group_id) }
+
+    it { should allow_value('email@address.foo').for(:email) }
+    it { should_not allow_value('email_address.foo').for(:email) }
+  end
+
+  context 'Associations' do
+    it { should belong_to(:group) }
+    it { should have_many(:events).with_foreign_key(:group_rep_id) }
+  end
+
 end
