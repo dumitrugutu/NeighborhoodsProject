ac844f67f4a3b3a4982a6de3f2198592313d0e55
Bryan Karlovitz <karlovitz@u.northwestern.edu>
2016-05-08 20:03:20 +0000

    test EventsController#create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,4 +9,17 @@ class EventsController < ApplicationController
   
   def new
   end
+  
+  def create
+    @event = Event.create!(event_params)
+    flash[:notice] = "#{@event.name} was created successfully."
+    redirect_to group_rep_events_path
+  end
+  
+  private
+  
+  def event_params
+    params.require(:event).permit(:group_rep_id, :name, :organizer_contact_info, 
+                                    :event_time, :is_free, :location)
+  end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -28,4 +28,31 @@ describe EventsController do
       expect(response).to render_template :new
     end
   end
+  
+  describe '#create' do
+    before do
+      @event_params = FactoryGirl.attributes_for(:event)
+      @event = instance_double('Event')
+    end
+    
+    it 'calls the model method that creates an event in the database' do
+      expect(Event).to receive(:create!).with(@event_params).and_return(@event)
+      allow(@event).to receive(:name).and_return('good time')
+      post :create, :group_rep_id => '1', :event => @event_params    # hardcode for now
+    end
+    
+    it 'gives the user a flash notice upon successful creation' do
+      allow(Event).to receive(:create!).with(@event_params).and_return(@event)
+      allow(@event).to receive(:name).and_return('good time')
+      post :create, :group_rep_id => '1', :event => @event_params  
+      expect(flash[:notice]).to be_present
+    end
+    
+    it 'redirects to the events index' do
+      allow(Event).to receive(:create!).with(@event_params).and_return(@event)
+      allow(@event).to receive(:name).and_return('good time')
+      post :create, :group_rep_id => '1', :event => @event_params
+      expect(response).to redirect_to group_rep_events_path
+    end
+  end
 end
