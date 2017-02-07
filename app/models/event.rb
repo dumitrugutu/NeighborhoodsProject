6573857b4cbc83524612837ec50f5b0e2165ab94
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-07 14:56:44 -0600

    Include the EventsHelper module

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
-  belongs_to :group_rep
+  include EventsHelper
 
+  belongs_to :group_rep
   validates_presence_of :name, :organizer_contact_info, :group_rep_id,
     :event_time, :is_free?
+
+
 end
