dc6d8bb5d95ef24c032a27ca41e1b75610369d66
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-07 10:03:17 -0600

    Finish updating the tests for events_controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -75,93 +75,76 @@ describe EventsController do
 
   context '#edit' do
     it 'calls the model method that finds the specified event' do
-      expect(Event).to receive(:find).and_return(true)
+      allow(Event).to receive(:find).and_return(true)
       get :edit, params: { group_rep_id: group_rep.id, id: 1 }  # hardcode for now
     end
 
     it 'renders the :edit view' do
       allow(Event).to receive(:find)
-
       get :edit, params: { group_rep_id: group_rep.id, id: 1 }  # hardcode for now
       expect(response).to render_template :edit
     end
   end
 
   context '#update' do
-    before do
-      @event_params = FactoryGirl.attributes_for(:event)
-      @event = instance_double('Event')
-    end
-
     it 'calls the model method to find the specified event' do
-      expect(Event).to receive(:find).and_return(@event)
-      allow(@event).to receive(:update!)
-      allow(@event).to receive(:name)
-
-      put :update, params: { group_rep_id: group_rep.id, id: 1, event: @event_params }   # hardcode for now
+      allow(Event).to receive(:find).and_return(event)
+      expect(event).to receive(:update!)
+      put :update, params: { group_rep_id: group_rep.id, id: 1, event: valid_event_params }   # hardcode for now
     end
 
-    it 'calls the model method to update the events attributes' do
-      allow(Event).to receive(:find).and_return(@event)
-      expect(@event).to receive(:update!)
-      allow(@event).to receive(:name)
-
-      put :update, params: { group_rep_id: group_rep.id, id: 1, event: @event_params }   # hardcode for now
+    it 'updates the event attributes' do
+      event = Event.create(valid_event_params)
+      allow(Event).to receive(:find).and_return(event)
+      expect(event).to receive(:update!)
+      event.name = "Test"
+      expect(event.name).to eq("Test")
+      put :update, params: { group_rep_id: group_rep.id, id: event.id, event: valid_event_params }   # hardcode for now
     end
 
     it 'gives the user a flash notice upon successful update' do
-      allow(Event).to receive(:find).and_return(@event)
-      allow(@event).to receive(:update!)
-      allow(@event).to receive(:name)
+      allow(Event).to receive(:find).and_return(event)
+      allow(event).to receive(:update!)
 
-      put :update, params: { group_rep_id: group_rep.id, id: 1, event: @event_params }   # hardcode for now
+      put :update, params: { group_rep_id: group_rep.id, id: 1, event: valid_event_params }   # hardcode for now
       expect(flash[:notice]).to be_present
     end
 
     it 'redirects to the events show' do
-      allow(Event).to receive(:find).and_return(@event)
-      allow(@event).to receive(:update!)
-      allow(@event).to receive(:name)
+      allow(Event).to receive(:find).and_return(event)
+      allow(event).to receive(:update!)
 
-      put :update, params: { group_rep_id: group_rep.id, id: 1, event: @event_params }  # hardcode for now
+      put :update, params: { group_rep_id: group_rep.id, id: 1, event: valid_event_params }  # hardcode for now
       expect(response).to redirect_to group_rep_event_path
     end
   end
 
   context '#destroy' do
-    before do
-      @event = instance_double('Event')
-    end
-
     it 'calls the model method to find the specified event' do
-      expect(Event).to receive(:find).and_return(@event)
-      allow(@event).to receive(:destroy)
-      allow(@event).to receive(:name)
+      event = Event.create(valid_event_params)
+      allow(Event).to receive(:find).and_return(event)
+      expect(event).to receive(:destroy)
 
-      delete :destroy, params: { group_rep_id: group_rep.id, id: 1 }  # hardcode for now
+      delete :destroy, params: { group_rep_id: group_rep.id, id: event.id }  # hardcode for now
     end
 
-    it 'calls the model method to delete the event' do
-      allow(Event).to receive(:find).and_return(@event)
-      expect(@event).to receive(:destroy)
-      allow(@event).to receive(:name)
-
-      delete :destroy, params: { group_rep_id: group_rep.id, id: 1 }  # hardcode for now
+    it 'it deletes the event' do
+      event = Event.create(valid_event_params)
+      allow(Event).to receive(:find).and_return(event)
+      expect { delete :destroy, params: { group_rep_id: group_rep.id, id: event.id }}.to change { Event.count }.from(1).to(0)
     end
 
     it 'gives the user a flash notice that the event was successfully deleted' do
-      allow(Event).to receive(:find).and_return(@event)
-      allow(@event).to receive(:destroy)
-      allow(@event).to receive(:name)
+      allow(Event).to receive(:find).and_return(event)
+      allow(event).to receive(:destroy)
 
       delete :destroy, params: { group_rep_id: group_rep.id, id: 1 }  # hardcode for now
       expect(flash[:notice]).to be_present
     end
 
     it 'redirects to the events index' do
-      allow(Event).to receive(:find).and_return(@event)
-      allow(@event).to receive(:destroy)
-      allow(@event).to receive(:name)
+      allow(Event).to receive(:find).and_return(event)
+      allow(event).to receive(:destroy)
 
       delete :destroy, params: { group_rep_id: group_rep.id, id: 1 }   # hardcode for now
       expect(response).to redirect_to group_rep_events_path
