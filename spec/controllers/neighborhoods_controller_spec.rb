c4ab10e2ed9541781547cafc92ca091ac73203cf
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-07 12:16:31 -0600

    Update tests for neighborhood_controller

--- spec/controllers/neighborhoods_controller_spec.rb
+++ spec/controllers/neighborhoods_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe NeighborhoodsController do
+  describe '#new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
   describe '#index' do
     it 'renders the index template' do
       get :index
@@ -9,16 +16,50 @@ describe NeighborhoodsController do
   end
 
   describe '#search' do
-    it 'renders the show template' do
-      get :search
-      expect(response).to render_template('show')
+    before do
+      Neighborhood.create(name: "Uptown")
+      Neighborhood.create(name: "Humbold Park")
+      Neighborhood.create(name: "Rogers Park")
+      Neighborhood.create(name: "Lincold Park")
+    end
+    context 'when given accurate name' do
+      it 'renders the show template' do
+        get :search, params: { query: "Uptown" }
+        expect(response).to render_template('show')
+      end
+    end
+    context 'when given inaccurate name' do
+      it 'renders the multiple template' do
+        get :search, params: { query: "Park" }
+        expect(response).to render_template('multiple')
+      end
+    end
+    context 'when given wrong name' do
+      it 'renders the error template' do
+        get :search, params: { query: "Test" }
+        expect(response).to render_template('error')
+      end
     end
   end
 
   describe '#show' do
     it 'renders the show template' do
-      get :show, params: { id: 1 }
+      neighborhood = Neighborhood.create(name: "Uptown")
+      get :show, params: { id: neighborhood.id  }
       expect(response).to render_template('show')
+    end
+  end
+
+  describe '#show_all' do
+    it 'renders the showall template' do
+      get :show_all
+      expect(response).to render_template('showall')
+    end
+    it 'shows all the neighborhoods' do
+      n1 = Neighborhood.create(name: "Rogers Park")
+      n2 = Neighborhood.create(name: "Lincold Park")
+      get :show_all
+      expect(Neighborhood.all).to contain_exactly(n1,n2)
     end
   end
 end
